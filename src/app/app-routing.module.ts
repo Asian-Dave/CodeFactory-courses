1f231c5e61b35898a9e5c8b7a34e6482135a720e
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-07-02 11:14:53 +0200

    added routing form fullcourse segment to full course page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullCoursePageComponent } from './full-course-page/full-course-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'full-course-page',
+    component: FullCoursePageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
