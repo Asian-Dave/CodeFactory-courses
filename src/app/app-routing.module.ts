b06cb58c31789bdb480b9550f2d27cf77f8939e1
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-07-03 09:53:33 +0200

    created home as starting page so any router link will be shown as a new page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FullCoursePageComponent } from './full-course-page/full-course-page.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'full-course-page',
     component: FullCoursePageComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
   },
 ];
 
