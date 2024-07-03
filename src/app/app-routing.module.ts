8900978e3a6174ebf697dc2f383ba2445de582c4
Andjelo SALTIN <sreten.andjelo.saltin@gmail.com>
2024-07-03 10:00:46 +0200

    added all routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FullCoursePageComponent } from './full-course-page/full-course-page.component';
 import { HomeComponent } from './home/home.component';
+import { CourseDetailsPageComponent } from './course-details-page/course-details-page.component';
+import { NewsPageComponent } from './news-page/news-page.component';
+import { ContactUsPageComponent } from './contact-us-page/contact-us-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 const routes: Routes = [
   {
@@ -11,6 +15,22 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'course-details-page',
+    component: CourseDetailsPageComponent,
+  },
+  {
+    path: 'news-page',
+    component: NewsPageComponent,
+  },
+  {
+    path: 'contact-us-page',
+    component: ContactUsPageComponent,
+  },
+  {
+    path: 'about-page',
+    component: AboutPageComponent,
   },
 ];
 
