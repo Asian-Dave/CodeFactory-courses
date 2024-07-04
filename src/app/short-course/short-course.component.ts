83c82cf05f0174fd9f0dc353b953b7d07f9d89a5
Ghaith <ghaithshihabi@gmail.com>
2024-07-04 07:37:09 +0200

    Short courses section added

--- src/app/short-course/short-course.component.ts
+++ src/app/short-course/short-course.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ICourse } from '../models/courses-model';
+import { courses } from '../models/Courses';
 
 @Component({
   selector: 'app-short-course',
   templateUrl: './short-course.component.html',
-  styleUrl: './short-course.component.css'
+  styleUrls: ['./short-course.component.css'],
 })
 export class ShortCourseComponent {
+  courses: ICourse[];
 
+  constructor() {
+    this.courses = this.getFilteredCourses();
+  }
+
+  getFilteredCourses(): ICourse[] {
+    const allowedIds = [3, 4, 5, 6];
+    return courses.filter((course) => allowedIds.includes(course.id));
+  }
 }
